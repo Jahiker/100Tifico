3547291ee1bec0466475eb470ba0a686c19cf404
Jahiker Rojas <52946797+Jahiker@users.noreply.github.com>
2022-01-20 23:15:09 -0400

    first commit

---
+++ src/utils/getData.js
@@ -0,0 +1,17 @@
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const getData = async function(id) {
+    const apiUrl = id ? `${API}${id}` : API;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        return data;
+
+    } catch (error) {
+        console.error('Fetch Error: ', error);
+    }
+}
+
+export default getData;
